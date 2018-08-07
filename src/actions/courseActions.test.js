bcd6b247b6effe6e0f601e9283f9a1a4685a98f0
wesley <wesleyyw8@gmail.com>
2018-08-07 01:28:35 +0100

    testing reducers

---
+++ src/actions/courseActions.test.js
@@ -0,0 +1,29 @@
+
+import expect from 'expect';
+import * as courseActions from './courseActions';
+import * as types from './actionTypes';
+
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+
+// Test a sync action
+describe('Course Actions', () => {
+  describe('createCourseSuccess', () => {
+    it('should create a CREATE_COURSE_SUCCESS action', () => {
+      // arrange
+      const course = {id: 'clean-code', title: 'Clean Code'};
+      const expectedAction = {
+        type: types.CREATE_COURSE_SUCCESS,
+        course: course
+      };
+
+      // act
+      const action = courseActions.createCourseSuccess(course);
+
+      // assert
+      expect(action).toEqual(expectedAction);
+    });
+  });
+
+});
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,9 +3,10 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import {authorsFormattedForDropdown} from '../../selectors/selectors';
 import toastr from 'toastr';
 
-class ManageCoursePage extends React.Component {
+export class ManageCoursePage extends React.Component {
   constructor(props, context) {
     super(props, context);
 
@@ -33,9 +34,23 @@ class ManageCoursePage extends React.Component {
     course[field] = event.target.value;
     return this.setState({course: course});
   }
+  courseFormIsValid() {
+    let formIsValid = true;
+    let errors = {};
 
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+    }
+    this.setState({errors: errors});
+    return formIsValid;
+  }
   saveCourse(event) {
     event.preventDefault();
+
+    if (!this.courseFormIsValid()) {
+      return;
+    }
     this.setState({
       saving: true
     });
@@ -102,16 +117,9 @@ function mapStateToProps(state, ownProps) {
     course = getCourseById(state.courses, courseId);
   }
 
-  const authorsFormattedForDropdown = state.authors.map(author => {
-    return {
-      value: author.id,
-      text: author.firstName + ' ' + author.lastName
-    };
-  });
-
   return {
     course,
-    authors: authorsFormattedForDropdown
+    authors: authorsFormattedForDropdown(state.authors)
   };
 }
 
