914e48a58f7b8358de66c4cb90caf9a44b8dc01a
wesley <wesleyyw8@gmail.com>
2018-08-08 23:08:36 +0100

    last tests done

---
+++ src/store/store.test.js
@@ -0,0 +1,23 @@
+import expect from 'expect';
+import {createStore} from 'redux';
+import rootReducer from '../reducers';
+import initialState from '../reducers/initialState';
+import * as courseActions from '../actions/courseActions';
+
+describe('Store', () => {
+  it('should handle creating courses', () => {
+    const store = createStore(rootReducer, initialState);
+    const course = {
+      title: 'clean code'
+    };
+    const action = courseActions.createCourseSuccess(course);
+    store.dispatch(action);
+
+    const actual = store.getState().courses[0];
+    const expected = {
+      title: 'clean code'
+    };
+
+    expect(actual).toEqual(expected);
+  });
+});
